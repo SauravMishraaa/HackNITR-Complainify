0e6f89e3c729f4a86ce6c7bd223335d44eb55d6e
Uditya Raj <udityaraj2002@gmail.com>
2023-01-07 19:40:49 +0530

    Added Post form

--- complainify-frontend/src/App.js
+++ complainify-frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Feed from "./pages/Feed.js";
 import Navbar from "./components/Navbar";
+import PostForm from "./components/PostForm.js";
 
 function App() {
   return (
     <div className="bg-gray-400 h-screen w-screen">
-      <Navbar />
-      <br />
-      <Feed />
+      {/* <Navbar /> */}
+      {/* <br /> */}
+      {/* <Feed /> */}
+      <PostForm/>
     </div>
   );
 }
